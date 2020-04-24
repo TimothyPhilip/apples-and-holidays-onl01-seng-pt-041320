52bd9787ead4ff6a904fd429deabb4a56f47c0e0
Philippe Timothe <philippetimothe@gmail.com>
2020-04-24 12:33:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,9 +79,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-     puts "#{season}"
+     
   holiday.each do |holiday_name, supply_array|
-     puts "#{holiday_name} #{supply_array}"
+     
      if supply_array == "BBQ"
        return holiday.keys
     end
